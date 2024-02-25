a4fe75b4064ca9b400d4ae266eb8421f760ae722
depu <87559657+mrwolv@users.noreply.github.com>
2024-02-25 17:49:56 +0530

    created prvoding section

--- src/app/components/Provided.js
+++ src/app/components/Provided.js
@@ -6,36 +6,43 @@ import { LuSofa } from "react-icons/lu";
 
 const Provided = () => {
   return (
-    <div className="px-5 mt-8 md:mx-20 flex flex-col md:flex-row gap-5 md:items-center ">
-      <div className="md:px-52">
-        <h1 className="text-[#333369] text-[1.8rem] md:text-[2.1rem] font-bold">
-          Amenities Provided
-        </h1>
-        <p className="text-[1rem] text-[#707070] ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
-          repellat quidem voluptas ipsa nobis laudantium doloribus molestias
-          fuga accusantium sunt.
-        </p>
+    <section className=" mt-8 px-5 md:px-[12rem]   ">
+      <div className=" flex flex-col md:flex-row gap-5 md:items-center md:justify-between">
+        <div className="md:flex md:flex-col md:items-start md:justify-between md:gap-4 md:px-[6rem] ">
+          <h1 className="text-[#333369] text-[1.8rem] md:text-[2.1rem] font-bold">
+            Amenities Provided
+          </h1>
+          <p className="text-[1rem] text-[#707070] md:pr-12 ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+            repellat quidem voluptas ipsa nobis laudantium doloribus molestias
+            fuga accusantium sunt.
+          </p>
+        </div>
+
+        <div className="md:flex md:flex-col md:mr-10 md:gap-10 md:px-12 md:py-5 ">
+          <div className="md:flex md:items-center md:justify-between md:gap-20">
+            <p className="flex flex-col gap-2 md:gap-4   md:justify-between  ">
+              <TbAirConditioning size={50} color="#F8669E" />
+              <span className="text-[#707070] ">Air Conditiner</span>
+            </p>
+            <p className="flex flex-col gap-2 justify-center  ">
+              <LuParkingCircle size={50} color="#F8669E" />
+              <span className="text-[#707070]">Free Parking</span>
+            </p>
+          </div>
+          <div className="md:flex md:items-center md:justify-between md:gap-20">
+            <p className="flex flex-col gap-2 md:gap-4   md:justify-between  ">
+              <TbAirConditioning size={50} color="#F8669E" />
+              <span className="text-[#707070] ">Air Conditiner</span>
+            </p>
+            <p className="flex flex-col gap-2 justify-center  ">
+              <LuParkingCircle size={50} color="#F8669E" />
+              <span className="text-[#707070]">Free Parking</span>
+            </p>
+          </div>
+        </div>
       </div>
-      <div className="grid grid-cols-2 md:justify-center md:gap-12 gap-2  px-2 md:px-10">
-        <p className="flex flex-col gap-2 justify-center">
-          <TbAirConditioning size={50} color="#F8669E" />
-          <span className="text-[#707070]">Air Conditiner</span>
-        </p>
-        <p className="flex flex-col gap-2 justify-center">
-          <LuParkingCircle size={50} color="#F8669E" />
-          <span className="text-[#707070]">Free Parking</span>
-        </p>
-        <p className="flex flex-col gap-2 justify-center">
-          <IoFastFoodOutline size={50} color="#F8669E" />
-          <span className="text-[#707070]">Food & Drinks</span>
-        </p>
-        <p className="flex flex-col gap-2 justify-center">
-          <LuSofa size={50} color="#F8669E" />
-          <span className="text-[#707070]">Bussiness Lounge </span>
-        </p>
-      </div>
-    </div>
+    </section>
   );
 };
 
